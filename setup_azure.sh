647ade2785aa3ee39a46e28af33d437e490670e1
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2023-10-14 18:32:32 +0000

    Remove not required files

--- setup_azure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash  
-AZURE_SUBSCRIPTION_ID=YOUR_SUBSCRIPTION_ID
-az ad sp create-for-rbac --name "CICD" --role contributor --scopes /subscriptions/$AZURE_SUBSCRIPTION_ID --sdk-auth
--- setup_kaggle_api.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-find . -name "kaggle.json" -exec mv {} ~/.kaggle/kaggle.json \;
-chmod 600 /root/.kaggle/kaggle.json
